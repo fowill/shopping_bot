350adce9e11e0904dccaff9b118eda71c82c01ab
Lion Wu <fowillwly@gmail.com>
2020-04-12 15:22:57 +0800

    config

---
+++ config.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+
+    #LUIS
+    LUIS_APP_ID = "48faffb1-babb-4810-ab86-587150ff3ce1"
+    LUIS_API_KEY = "7dd9b99dbb6b40d795a932bcf10b2562"
+    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    LUIS_API_HOST_NAME = "westus.api.cognitive.microsoft.com"
+
+    #QNA 
+    QNA_KNOWLEDGEBASE_ID = os.environ.get("QnAKnowledgebaseId", "")
+    QNA_ENDPOINT_KEY = os.environ.get("QnAEndpointKey", "")
+    QNA_ENDPOINT_HOST = os.environ.get("QnAEndpointHostName", "")
